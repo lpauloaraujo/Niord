88d56484626e245e72da41d90bd2b964ec083f1d
LuizzDutra <luizzidutra@gmail.com>
2026-03-17 17:09:35 -0300

    feature:Basic overlay function/ extra:Demo

--- Niord/app/src/main/java/com/example/niord/MainActivity.kt
+++ Niord/app/src/main/java/com/example/niord/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.niord
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,8 +15,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.niord.ui.theme.NiordTheme
+import android.os.Build
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.net.toUri
+
+
+
+
 
 class MainActivity : ComponentActivity() {
+    private lateinit var overlayManager: OverlayManager
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    //Overlay permission trigger
+    private val permissionLauncherShowOverlay = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        // After returning from settings, check if permission is granted
+        if (Settings.canDrawOverlays(this)) {
+            overlayManager.show()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,18 +50,42 @@ class MainActivity : ComponentActivity() {
                         Greeting(
                             name = "Luz"
                         )
-                        Button(onClick = { println("Hello") }) {
+                        //Overlay Demo
+                        Button(onClick = {
+                            if (Settings.canDrawOverlays(applicationContext)) {
+                                if(overlayManager.showing()){
+                                    overlayManager.hide()
+                                }else {
+                                    overlayManager.show()
+                                }
+                            } else {
+                                // Open settings to grant permission
+                                val intent = Intent(
+                                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                    "package:$packageName".toUri()
+                                )
+                                permissionLauncherShowOverlay.launch(intent)
+                            }
+                        }) {
                             Text(
                                 text = "Hi"
                             )
                         }
-
                     }
                 }
             }
         }
+        overlayManager = OverlayManager(this)
+        overlayManager.show()
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onDestroy() {
+        overlayManager.hide() // Always remove the view when activity is destroyed
+        super.onDestroy()
     }
 }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
---
+++ Niord/app/src/main/java/com/example/niord/Overlay.kt
@@ -0,0 +1,132 @@
+package com.example.niord
+
+import android.content.Context
+import android.graphics.PixelFormat
+import android.os.Build
+import android.view.Gravity
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.example.niord.ui.theme.NiordTheme
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.lifecycle.setViewTreeViewModelStoreOwner
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+
+class OverlayManager(private val context: Context){
+    private var winManager: WindowManager? = null
+    init {
+        winManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    }
+    private var isShowing = false
+    private var floatingView: ComposeView? = null
+    private var lifecycleOwner: FloatingLifecycleOwner? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val layoutParams = WindowManager.LayoutParams().apply {
+        format = PixelFormat.TRANSLUCENT
+        flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+        type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        gravity = Gravity.CENTER
+        width = WindowManager.LayoutParams.WRAP_CONTENT
+        height = WindowManager.LayoutParams.WRAP_CONTENT
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun show(){
+        if(isShowing){return}
+
+        lifecycleOwner = FloatingLifecycleOwner().apply {
+            onCreate()
+            onResume()
+        }
+
+        floatingView = ComposeView(context.applicationContext).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnDetachedFromWindow)
+            setViewTreeLifecycleOwner(lifecycleOwner)
+            setViewTreeViewModelStoreOwner(lifecycleOwner)
+            setViewTreeSavedStateRegistryOwner(lifecycleOwner)
+            setContent {
+                NiordTheme {
+                    Floating(text = "Hello float")
+                }
+            }
+        }
+        floatingView?.let { view ->
+            try {
+                winManager?.addView(view, layoutParams)
+                isShowing = true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                // Handle errors (e.g., missing permission)
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun hide(){
+        if(!isShowing){return}
+        floatingView?.let { view ->
+            try {
+                winManager?.removeView(view)
+                isShowing = false
+                lifecycleOwner?.onDestroy()
+                lifecycleOwner = null
+            } catch (e: Exception) {
+                e.printStackTrace()
+                // Handle errors (e.g., missing permission)
+            }
+        }
+    }
+
+    fun showing(): Boolean {return isShowing}
+}
+
+
+
+class FloatingLifecycleOwner : LifecycleOwner, ViewModelStoreOwner, SavedStateRegistryOwner {
+
+    private val lifecycleRegistry = LifecycleRegistry(this)
+    private val store = ViewModelStore()
+    private val controller = SavedStateRegistryController.create(this)
+
+    override val lifecycle: Lifecycle = lifecycleRegistry
+    override val viewModelStore: ViewModelStore = store
+    override val savedStateRegistry: SavedStateRegistry = controller.savedStateRegistry
+
+    fun onCreate() {
+        controller.performRestore(null)
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    }
+
+    fun onResume() {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    }
+
+    fun onDestroy() {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        store.clear()
+    }
+}
+
+@Composable
+fun Floating(text: String){
+    Button(onClick = {println("Overlay Button Clicked")}
+    ){
+        Text(
+            text=text
+        )
+    }
+}
