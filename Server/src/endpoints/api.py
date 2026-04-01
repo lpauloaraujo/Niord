6b33f8bf99343859d036a727fba1040b70a3b6f4
LuizzDutra <luizzidutra@gmail.com>
2026-04-01 19:43:50 -0300

    development/Estrutura inicial do db e endpoints

--- Server/src/endpoints/api.py
+++ Server/src/endpoints/api.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
+from .user import router as user_router
 
-api_router = APIRouter(prefix='/test')
+api_router = APIRouter(prefix='')
+#All routers
+api_router.include_router(user_router)
 
-@api_router.get('/')
-def test():
+@api_router.get('/greet')
+def greet():
     return "Hello, API world!"
---
+++ Server/src/models/seguradora.py
@@ -0,0 +1,12 @@
+from .db import Base
+from sqlalchemy.orm import mapped_column, Mapped
+
+
+
+class Seguradora(Base):
+    __tablename__ = "seguradora"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+
+
