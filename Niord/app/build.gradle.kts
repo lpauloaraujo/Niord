27f39c06acac2f54fede2e6db89db591bee81151
LuizzDutra <luizzidutra@gmail.com>
2026-04-12 11:30:46 -0300

    Request system complete

--- Niord/app/build.gradle.kts
+++ Niord/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 android {
@@ -66,4 +67,5 @@ dependencies {
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.client.logging)
+    implementation(libs.kotlinx.serialization.json)
 }
--- Niord/app/src/main/java/com/example/niord/api/Models.kt
+++ Niord/app/src/main/java/com/example/niord/api/Models.kt
@@ -1,5 +1,6 @@
 package com.example.niord.api
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -10,6 +11,25 @@ data class Tokens(
 
 
 @Serializable
-data class GreetString(
-    val text: String
+data class ErrorResponse(
+    val detail: ErrorDetail
+)
+
+@Serializable
+data class ErrorDetail(
+    val message: String,
+    val type: String,
+    val field: String? = null
+)
+@Serializable
+data class RegisterPost(
+    val name: String,
+    val email: String,
+    val password: String,
+    @SerialName("registration_plate")
+    val registrationPlate: String,
+    val cpf: String,
+    val telephone: String,
+    @SerialName("blood_type")
+    val bloodType: String?
 )
