cf830f739fe8883ada8cbabe624f758591657c87
Morpheus720 <guilhemealves32isgone@gmail.com>
2026-06-09 11:14:58 -0300

    Correção do fluxo das seguradoras

--- Niord/app/src/main/java/com/example/niord/ConfiguracaoActivity.kt
+++ Niord/app/src/main/java/com/example/niord/ConfiguracaoActivity.kt
@@ -215,7 +215,7 @@ class ConfiguracaoActivity : ComponentActivity() {
     }
 
     private fun openSeguradorasFlow() {
-        startActivity(Intent(this, SeguradoraInicialActivity::class.java))
+        startActivity(Intent(this, SeguradoraFeedActivity::class.java))
     }
 
     private fun applyOverlayEnabledState(
